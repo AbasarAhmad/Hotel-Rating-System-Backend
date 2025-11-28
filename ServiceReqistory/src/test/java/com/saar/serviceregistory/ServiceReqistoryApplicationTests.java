f4ec19ad0169935bdf3f3fcf7f65a93035f29b89
absar <absarbhu854326@gmail.com>
2025-11-28 15:30:05 +0530

    ServiceRegistory(Implemented Eureka Server) is working on port 8761

---
+++ ServiceReqistory/src/test/java/com/saar/serviceregistory/ServiceReqistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saar.serviceregistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceReqistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
